f3ad768ced73298f6511cc3d5819c297bcdbaf2f
Levi Scott <121728481+LeviScott29@users.noreply.github.com>
2023-12-27 16:51:28 -0500

    Add files via upload
    This program asks for an input of the city you grew up in and the name of your pet then uses the inputs and concatenates them to create your band name.

---
+++ band generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city= input("What's the name of the city you grew up in? ")
+pet=input("What is the name of your favorite pet? ")
+print("your band name is" + " " + city + " " +pet)
