0ca770f798bf7d12db470b0ea8d3e6ce382afe37
Levi Scott <121728481+LeviScott29@users.noreply.github.com>
2023-12-27 17:29:19 -0500

    Add files via upload

---
+++ tip calculator.py
@@ -0,0 +1,11 @@
+print("welcome to the tip calculator")
+bill= float(input ("What was the total bill? "))
+people= int(input("how many people were there? "))
+tip = float(input (" what percentage would you like to tip? "))
+
+tip_by_percent= tip/100
+print(tip_by_percent)
+total_bill= (bill)+(bill*tip_by_percent)
+print(total_bill)
+split_bill= total_bill/people
+print ("each person owes:", split_bill)
