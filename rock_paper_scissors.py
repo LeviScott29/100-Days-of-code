ded2e6564b10af027fe4247e7bf10e75a54137a7
Levi Scott <121728481+LeviScott29@users.noreply.github.com>
2023-12-31 12:41:20 -0500

    Add files via upload

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,27 +1,37 @@
 import random
 
 print("lets play rock paper scissors")
+choices = ["rock", "paper", "scissors"]
 while True:
     user_input= int(input ("pick 0 for rock, 1 for paper, 2 for scissors or exit to quit: "))
-    print ("you chose: ", user_input)
-
 
     computer_input= random.randint(0,2)
     computer_choice= computer_input
 
-    print("computer chose: ", computer_choice)
     if computer_choice > user_input:
         if user_input == 0 and computer_choice == 2:
+            print("you chose: ", choices[user_input])
+            print("computer chose: ", choices[computer_input])
             print("you win")
-        else: print("you lose")
+        else:
+            print("you chose: ", choices[user_input])
+            print("computer chose: ", choices[computer_input])
+            print("you lose")
                     
     elif user_input > computer_choice:
         if computer_choice== 0 and user_input == 2:
+            print("you chose: ", choices[user_input])
+            print("computer chose: ", choices[computer_input])
             print(" you lose")
-        else: print("you win")
+        else:
+            print("you chose: ", choices[user_input])
+            print("computer chose: ", choices[computer_input])
+            print("you win")
                     
     elif computer_choice == user_input:
-                    print("you tied")
+        print("you chose: ", choices[user_input])
+        print("computer chose: ", choices[computer_input])
+        print("you tied")
     else: print("invalid input")
                   
         
