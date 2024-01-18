73a3c84fedd08d3ed96a7bd753a271ac68bd60f1
Levi Scott <121728481+LeviScott29@users.noreply.github.com>
2024-01-17 19:36:23 -0500

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,28 +1,23 @@
 import turtle
 from turtle import Turtle, dot, Screen
 import random
+#set screen and turtle characteristics
 screen= Screen()
 timmy= Turtle()
 timmy.shape("turtle")
 timmy.color("red")
+screen.screensize(400,400)
+screen.colormode(255)
 choices = random
-direction = [0, 90, 180, 270]
 
 
 def random_color():
     return choices.randint(0,255)
-
-
-screen.colormode(255)
-
-color1 = random_color()
-color2 = random_color()
-color3 = random_color()
-timmy.pencolor(color1, color2, color3)
-screen.screensize(400,400)
+#set pen color and turtle speed
 timmy.pensize(1)
 timmy.speed(0)
 
+#function to choose random point on screed and make spiralgraph drawing with random colors for each circle then pick another spot
 for _ in range(10):
     timmy.penup()
     timmy.setposition(choices.randint(-350, 350), choices.randint(-350, 350))
